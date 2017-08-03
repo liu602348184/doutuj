f9c15c047f0abcbc6895f978fe8b5e90b3af8d5f
liu602348184 <602348184@qq.com>
2017-08-03 18:16:28 +0800

    add baidu counting

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -3,6 +3,15 @@
 					<p><img src="{{ env('APP_URL') }}/img/logo.png" alt=""></p>
 					<p>版权声明：本站资源均来自于互联网，其著作权归原作者所有，如果有侵犯您的权益，请来信告知，我们将尽快作出处理。</p>
 					<p>联系我们：602348184@qq.com</p>
+					<script>
+						var _hmt = _hmt || [];
+						(function() {
+							var hm = document.createElement("script");
+							hm.src = "https://hm.baidu.com/hm.js?b65cb63dcf130578d0a512d90e96405e";
+							var s = document.getElementsByTagName("script")[0]; 
+							s.parentNode.insertBefore(hm, s);
+						})();
+					</script>
 				</div>
 			</footer>
 			<!--[if lt IE 9]>
