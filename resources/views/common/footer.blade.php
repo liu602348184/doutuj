8e637cbc93d055ecda3acc46765ff2303beb4afd
liu602348184 <602348184@qq.com>
2017-08-03 23:26:21 +0800

    removing 360 login

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -3,7 +3,6 @@
 					<p><img src="{{ env('APP_URL') }}/img/logo.png" alt=""></p>
 					<p>版权声明：本站资源均来自于互联网，其著作权归原作者所有，如果有侵犯您的权益，请来信告知，我们将尽快作出处理。</p>
 					<p>联系我们：602348184@qq.com</p>
-					<p><a href="http://webscan.360.cn/index/checkwebsite/url/doutuj.com"><img border="0" src="http://img.webscan.360.cn/status/pai/hash/36f07bcba656bcdd6e7453a56d87f14f"/></a></p>
 					<script>
 						var _hmt = _hmt || [];
 						(function() {
