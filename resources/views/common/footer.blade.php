57e25293a95a1c62ce71425c3112fec0180ce21a
liu602348184 <602348184@qq.com>
2017-08-04 19:50:37 +0800

    adding icp number

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -3,6 +3,7 @@
 					<p><img src="{{ env('APP_URL') }}/img/logo.png" alt=""></p>
 					<p>版权声明：本站资源均来自于互联网，其著作权归原作者所有，如果有侵犯您的权益，请来信告知，我们将尽快作出处理。</p>
 					<p>联系我们：602348184@qq.com</p>
+					<p>苏ICP备17044940号-1</p>
 					<script>
 						var _hmt = _hmt || [];
 						(function() {
