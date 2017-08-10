017dd8b8a463ab2d2f1c8062d6e6ff2870858a95
liu602348184 <602348184@qq.com>
2017-08-10 13:22:08 +0800

    adding baidu pushing

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -12,6 +12,21 @@
 							var s = document.getElementsByTagName("script")[0]; 
 							s.parentNode.insertBefore(hm, s);
 						})();
+
+						/* baidu pushing */
+						(function(){
+						    var bp = document.createElement('script');
+						    var curProtocol = window.location.protocol.split(':')[0];
+						    if (curProtocol === 'https') {
+						        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+						    }
+						    else {
+						        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+						    }
+						    var s = document.getElementsByTagName("script")[0];
+						    s.parentNode.insertBefore(bp, s);
+						})();
+						/* baidu pushing */
 					</script>
 				</div>
 			</footer>
